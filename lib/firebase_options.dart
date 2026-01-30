7f67af25ab5bd37464879f484e57f3570dbc2d01
ahmed <3lbashmohands@gmail.com>
2026-01-31 00:23:24 +0200

    add firebase options and update main

---
+++ lib/firebase_options.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get web {
+    return const FirebaseOptions(
+      apiKey: "AIzaSyCM-44qaAjY1ahwR93XTNc9X0eHp07bl1c",
+      authDomain: "archive-system-2100d.firebaseapp.com",
+      projectId: "archive-system-2100d",
+      storageBucket: "archive-system-2100d.firebasestorage.app",
+      messagingSenderId: "150230259801",
+      appId: "1:150230259801:web:39712cdc8298b628213948",
+    );
+  }
+}
